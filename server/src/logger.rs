bd5e024328b8314e0b46c86b115a25b60c844986
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-04-11 19:02:28 +0200

    chore: format code

--- server/src/logger.rs
+++ server/src/logger.rs
@@ -6,7 +6,9 @@ const LOG_FILE_DIR_NAME: &str = "markdown-lsp-logs";
 
 // XXX I believe logs go to neovim if they are printed on stderr
 pub fn init_logger() -> WorkerGuard {
-    let home = get_my_home().expect("could not get HOME dir").expect("no HOME dir set");
+    let home = get_my_home()
+        .expect("could not get HOME dir")
+        .expect("no HOME dir set");
     let mut log_file_dir = home;
     log_file_dir.push(LOG_FILE_DIR_NAME);
     let file_appender = tracing_appender::rolling::never(log_file_dir, LOG_FILE_NAME);
